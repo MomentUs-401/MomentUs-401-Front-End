dc417edf4e2d28621eb4a182a07f2e08c888989b
Allie Grampa <agrampa@yahoo.com>
2017-06-20 08:16:42 -0700

    completed scaffold for navbar test

---
+++ test/component/navbar-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Testing the navigation bar controller', function () {
+  beforeEach(() => {
+    angular.mock.module('momentus');
+    angular.mock.inject(($rootScope, $componentController, $location, $window, authService) => {
+      this.$rootScope = $rootScope;
+      this.$scope = this.$rootScope.$new();
+      this.$window = $window;
+      this.$location = $location;
+      this.authService = authService;
+      this.$componentController = $componentController;
+      this.$window.localStorate.setItem('token', 'test token');
+      
+      this.navbarCtrl = this.$componentController('navbarController',
+        {
+          scope: this.$scope,
+          authService: this.authService,
+        }
+      );
+      
+      this.navbarCtrl.$onInit();
+    });
+  });
+  
+  afterEach(() => {
+    this.$rootScope.$apply();
+    this.$window.localStorage.removeItem('token');
+  });
+  
+  it('should have a method to check the path', () => {
+    expect(this.navbarCtrl).to.be.a('function');
+  });
+  
+  it('should have a method for the user to log out', () => {
+    expect(this.navbarCtrl.logout).to.be.a('function');
+  });
+});
